1649c93c8b136d5b65d3c1e3db0bdfd565ef3a4e
Matthew Bailey <mmchy6@gmail.com>
2024-04-15 10:38:46 -0600

    run

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -31,6 +31,8 @@ public class Server {
             throw new RuntimeException(e);
         }
     }
+
+
     public int run(int desiredPort) {
         Spark.port(desiredPort);
         Spark.staticFiles.location("web");
@@ -39,9 +41,24 @@ public class Server {
 
         var gson = new Gson();
 
-        // Clear application endpoint
-        Spark.delete("/db", (req, res) -> {
+        configureEndpoints(gson);
 
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    private void configureEndpoints(Gson gson) {
+        configureClearDBEndpoint(gson);
+        configureRegisterEndpoint(gson);
+        configureLoginEndpoint(gson);
+        configureLogoutEndpoint(gson);
+        configureListGamesEndpoint(gson);
+        configureCreateGameEndpoint(gson);
+        configureJoinGameEndpoint(gson);
+    }
+
+    private void configureClearDBEndpoint(Gson gson) {
+        Spark.delete("/db", (req, res) -> {
             try {
                 service.clearDB();
             } catch (DataAccessException e) {
@@ -52,15 +69,16 @@ public class Server {
 
             return "{}";
         });
+    }
 
-        // Register endpoint
+    private void configureRegisterEndpoint(Gson gson) {
         Spark.post("/user", (req, res) -> {
             RegisterRequest registerRequest = gson.fromJson(req.body(), RegisterRequest.class);
 
             try {
                 RegisterResponse registerResponse = service.register(registerRequest);
                 return gson.toJson(registerResponse);
-            } catch (BadRequestException e) { // throw in service, when parameter is null (or not white/black for playerColor)
+            } catch (BadRequestException e) {
                 ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage());
                 res.status(400);
                 return gson.toJson(exceptionResponse);
@@ -73,17 +91,17 @@ public class Server {
                 res.status(500);
                 return gson.toJson(exceptionResponse);
             }
-
         });
+    }
 
-        // Login endpoint
+    private void configureLoginEndpoint(Gson gson) {
         Spark.post("/session", (req, res) -> {
             LoginRequest loginRequest = gson.fromJson(req.body(), LoginRequest.class);
 
             try {
                 LoginResponse loginResponse = service.login(loginRequest);
                 return gson.toJson(loginResponse);
-            } catch (UnauthorizedException e) { // return if authToken is not found
+            } catch (UnauthorizedException e) {
                 ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage());
                 res.status(401);
                 return gson.toJson(exceptionResponse);
@@ -93,8 +111,9 @@ public class Server {
                 return gson.toJson(exceptionResponse);
             }
         });
+    }
 
-        // Logout endpoint
+    private void configureLogoutEndpoint(Gson gson) {
         Spark.delete("/session", (req, res) -> {
             LogoutRequest logoutRequest = new LogoutRequest(req.headers("Authorization"));
             try {
@@ -110,8 +129,9 @@ public class Server {
                 return gson.toJson(exceptionResponse);
             }
         });
+    }
 
-        // List Games endpoint
+    private void configureListGamesEndpoint(Gson gson) {
         Spark.get("/game", (req, res) -> {
             ListGamesRequest listGamesRequest = new ListGamesRequest(req.headers("Authorization"));
             try {
@@ -126,10 +146,10 @@ public class Server {
                 res.status(500);
                 return gson.toJson(exceptionResponse);
             }
-
         });
+    }
 
-        // Create Game endpoint
+    private void configureCreateGameEndpoint(Gson gson) {
         Spark.post("/game", (req, res) -> {
             CreateGameRequest createGameRequest = gson.fromJson(req.body(), CreateGameRequest.class);
             createGameRequest.setAuthToken(req.headers("Authorization"));
@@ -150,16 +170,14 @@ public class Server {
                 return gson.toJson(exceptionResponse);
             }
         });
+    }
 
-        // Join Game endpoint
+    private void configureJoinGameEndpoint(Gson gson) {
         Spark.put("/game", (req, res) -> {
             JoinGameRequest joinGameRequest = gson.fromJson(req.body(), JoinGameRequest.class);
             joinGameRequest.setAuthToken(req.headers("Authorization"));
             try {
                 service.joinGame(joinGameRequest);
-//                shouldn't need to have this following line, websocketfacade should send ws message and websockethandler
-//                should receive it automatically
-//                webSocketHandler.joinPlayer(joinGameRequest.getPlayer); // need some way to get name
                 return "{}";
             } catch (BadRequestException e) {
                 ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage());
@@ -179,9 +197,6 @@ public class Server {
                 return gson.toJson(exceptionResponse);
             }
         });
-
-        Spark.awaitInitialization();
-        return Spark.port();
     }
 
     public int port() {
