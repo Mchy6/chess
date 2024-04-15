855e06cd0d16f6e7bd47c96ee1e790203d38a9a0
Matthew Bailey <mmchy6@gmail.com>
2024-04-15 10:08:46 -0600

    fixed error

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,14 +1,8 @@
-import dataAccess.DataAccessException;
 import exception.ResponseException;
-import server.Server;
 import ui.Repl;
 
 public class Main {
     public static void main(String[] args) throws ResponseException {
-        Server server = null;
-        server = new Server();
-        server.run(8080);
-
         var serverUrl = "http://localhost:8080";
         if (args.length == 1) {
             serverUrl = args[0];
